0ce7ab1862ced8d34c7385fa5f01d1d5ce566dd9
Renee-W <missmrwhite12@gmail.com>
2021-04-11 23:10:18 -0600

    update panda and add files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python-api-challenge
+Download files needed for challenge
+get api keyh for weather and google
+start juptyer notebook file
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "93a1b7a56ffa925f17f9c3d79187a182"
+
+# Google API Key
+g_key = "AIzaSyBZkw5TGRPnPxyf2Qvsdc5qvk7ogRfHkVQ"
